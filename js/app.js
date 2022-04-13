7cc82accec357f48fb2f7ec5b7c93db57d794ed3
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2022-04-12 22:21:23 -0600

    Crea clase Presupuesto y UI
    - En clase de Presupusto se agrego un constructor y se instancio con una variable global

--- js/app.js
+++ js/app.js
@@ -9,11 +9,24 @@ function eventListeners() {
 }
 
 // Clases
+class Presupuesto {
+  constructor(presupuesto) {
+    this.presupuesto = Number(presupuesto);
+    this.restante = Number(this.restante);
+    this.gastos = [];
+  }
+}
+class UI {}
+
+// Instanciar.
+const ui = new UI();
+
+let presupuesto;
 
 // Funciones
 function preguntarPresupuesto() {
   const presupuestoUsuario = prompt("¿Cual es tu presupuesto?");
-  console.log(Number(presupuestoUsuario));
+  // console.log(Number(presupuestoUsuario));
 
   // Validar que no este vacio, que no sea un null, que no sea un String  y que no sea 0 o negativo
   if (
@@ -24,4 +37,8 @@ function preguntarPresupuesto() {
   ) {
     window.location.reload();
   }
+
+  // Presupuesto Valido
+  presupuesto = new Presupuesto(presupuestoUsuario);
+  console.log(presupuesto);
 }
