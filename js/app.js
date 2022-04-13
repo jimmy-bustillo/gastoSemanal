1ba278b029de32f0b74f21ef03cb5100675b25e9
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2022-04-12 22:49:03 -0600

    Crea 2 métodos en la clase de UI para imprimir en el HTML
    - insertarPresupuesto toma la cantidad del valor de presupuesto para agregarlos en le HTML
    - imprimirAlerta toma dos parametros; (mensaje) es el que se imprime en el HTML como un div y
    (tipo) le da los estilos al mensaje dependiendo de su tipo (error o success)

--- js/app.js
+++ js/app.js
@@ -6,17 +6,50 @@ const gastoListado = document.querySelector("#gastos ul");
 eventListeners();
 function eventListeners() {
   document.addEventListener("DOMContentLoaded", preguntarPresupuesto);
+
+  formulario.addEventListener("submit", agregarGasto);
 }
 
 // Clases
 class Presupuesto {
   constructor(presupuesto) {
     this.presupuesto = Number(presupuesto);
-    this.restante = Number(this.restante);
+    this.restante = Number(presupuesto);
     this.gastos = [];
   }
 }
-class UI {}
+class UI {
+  insertarPresupuesto(cantidad) {
+    // Extrayendo el valor
+    const { presupuesto, restante } = cantidad;
+
+    // Agregar al HTML
+    document.querySelector("#total").textContent = presupuesto;
+    document.querySelector("#restante").textContent = restante;
+  }
+
+  imprimirAlerta(mensaje, tipo) {
+    // Crear el DIV
+    const divMensaje = document.createElement("div");
+    divMensaje.classList.add("text-center", "alert");
+    if (tipo === "error") {
+      divMensaje.classList.add("alert-danger");
+    } else {
+      divMensaje.classList.add("alert-success");
+    }
+
+    // Mensaje de error
+    divMensaje.textContent = mensaje;
+
+    // Insertar en el HTML
+    document.querySelector(".primario").insertBefore(divMensaje, formulario);
+
+    // Quitar del HTML
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
+  }
+}
 
 // Instanciar.
 const ui = new UI();
@@ -41,4 +74,25 @@ function preguntarPresupuesto() {
   // Presupuesto Valido
   presupuesto = new Presupuesto(presupuestoUsuario);
   console.log(presupuesto);
+
+  ui.insertarPresupuesto(presupuesto);
+}
+
+// Añade gastos
+function agregarGasto(e) {
+  e.preventDefault();
+
+  // Leer los datos del formulario
+  const nombre = document.querySelector("#gasto").value;
+  const cantidad = document.querySelector("#cantidad").value;
+
+  if (nombre === "" || cantidad === "") {
+    ui.imprimirAlerta("Ambos campos son obligatorios", "error");
+    return;
+  } else if (cantidad <= 0 || isNaN(cantidad)) {
+    ui.imprimirAlerta("Cantidad no válida", "error");
+    return;
+  }
+
+  console.log("Agregando gasto...");
 }
